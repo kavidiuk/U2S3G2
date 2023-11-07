6661c9962d8ff758d84f3f0dc93cc1e69a825df7
kavidu <kaviduwarnakulasuriya@gmail.com>
2023-11-07 16:56:47 +0100

    first-commit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-window.onload();
+window.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("form");
+  form.onsubmit = saveData;
+});
+
+const saveData = (event) => {
+  event.preventDefault();
+  const input = document.getElementById("inputUtente");
+  const text = input.value;
+  localStorage.setItem("data", text);
+};
